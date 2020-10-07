878e23840d5dde7d7a80bb2fa5d4733f1d92797b
khalil <elmaghraoui.khalil.1@gmail.com>
2020-10-07 17:05:10 +0100

    Update main.js

--- javascriptt/main.js
+++ javascriptt/main.js
@@ -1,36 +1,75 @@
-// grap the input value //
+// One Month JS
+// Course Link
+
+// Week 2 Homework
+// Finish the GIF search engine
 
 
 
+// 1. grab the input value
 
 document.querySelector(".js-go").addEventListener('click', function() {
-    //  alert("test");
 
-    var x = document.querySelector("input").value;
-    console.log(x);
-    Push(x);
+    var input = document.querySelector("input").value;
+    pushToAPI(input);
 
+
+
+});
+
+document.querySelector(".js-userinput").addEventListener('keyup', function(e) {
+
+    var input = document.querySelector("input").value;
+
+    if (e.which === 13) {
+        pushToAPI(input);
+    }
 
 });
 
 
 
+// 2. do the data stuff with the API
+
+function pushToAPI(input) {
+
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=dc6zaTOxFJmzC"
+
+    console.log(url);
+
+    //AJAX Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open('Get', url);
+    GiphyAJAXCall.send();
+
+    GiphyAJAXCall.addEventListener('load', function(e) {
+
+        var data = e.target.response;
+        pushToDOM(data);
+
+    });
+
+}
 
 
 
+//3. return and show the GIFs
 
+function pushToDOM(data) {
 
-// do the data stuff with the API //
-
-
-
-
-
-
-
-// show me the GIFS //
-
-function Push(x) {
     var container = document.querySelector(".js-container");
-    container.innerHTML = x;
+
+    var response = JSON.parse(data);
+
+    var imageUrls = response.data;
+
+    container.innerHTML = "";
+
+    imageUrls.forEach(function(image) {
+
+        var gifUrl = image.images.fixed_height.url;
+
+        container.innerHTML += "<img src=\"" + gifUrl + "\" class=\"container-image\">";
+
+    });
 }
